2e10aa7e1fd4e1625f42a704871b01416110cb29
IPKIN9 <irwandirini@gmail.com>
2021-12-20 09:51:27 +0800

    Choice message function

--- resources/views/cms/message.blade.php
+++ resources/views/cms/message.blade.php
@@ -5,6 +5,11 @@
             <p class="text-danger">Periksa inputan anda sebelum mengirim</p>
         </div>
     @endif
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('status') }}
+        </div>
+    @endif
     @if (session('status'))
         <div class="alert alert-success">
             {{ session('status') }}
